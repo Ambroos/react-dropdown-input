79a9b7415af13ffa24a1998a30f556c3e772f6ad
Ambroos Vaes <ambroos@wieni.be>
2015-05-19 11:57:10 +0200

    Rebuild

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ var DropdownInput = React.createClass({
     // the rest are to make eslint happy
     id: React.PropTypes.string,
     className: React.PropTypes.string,
-    bsSize: React.PropTypes.string
+    bsSize: React.PropTypes.string,
+    noHighlight: React.PropTypes.bool
   },
 
   getInitialState: function getInitialState() {
@@ -150,7 +151,7 @@ var DropdownInput = React.createClass({
         part2 = item.slice(start, end),
         part3 = item.slice(end);
     var classes = cx({ active: this.state.activeIndex === index, disabled: disabled === true });
-    if (disabled) {
+    if (disabled || this.props.noHighlight) {
       // don't highlight parts of disabled items, eg. the maxText
       part1 = item;
       part2 = null;
